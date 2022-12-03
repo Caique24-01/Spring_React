d61981c008b39b6d8e73ffb992a8fcc69ff9d3a5
Caique24-01 <caiqueleme100@gmail.com>
2022-12-02 21:01:31 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from "axios";
 import notificationIcon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function NotificationButton({ saleId }: Props) {
+  function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+      console.log("Success!")
+    })
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={notificationIcon} alt="Notificar" />
     </div>
   );
